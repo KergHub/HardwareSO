ed21a517173163eded1cebe97c463b5a73ff2323
yucchava <yucchava@bancolombia.com.co>
2022-11-09 23:37:36 -0500

    initial commit

---
+++ HardwareSO/Disco.cs
@@ -0,0 +1,29 @@
+﻿namespace HardwareSO
+{
+    public class Disco
+    {
+        public static string unidadDisco()
+        {
+
+            DriveInfo[] Discos = DriveInfo.GetDrives();
+            string discos = "";
+            foreach (DriveInfo disk in Discos)
+            {
+                discos += ($"Drive {disk.Name} \n\r" +
+                $"Drive type: {disk.DriveType} \n\r"
+                );
+                if (disk.IsReady == true)
+                {
+                    discos += ($"Etiqueta de volumen: {disk.VolumeLabel} \n\r" +
+                        $"Sistema de archivos: {disk.DriveFormat} \n\r" +
+                        $"Espacio disponible para el usuario actual: {Convert.ToDecimal(disk.AvailableFreeSpace) / 1073741824} GB \n\r" +
+                        $"Espacio total disponible: {Convert.ToDecimal(disk.TotalFreeSpace) / 1073741824} GB \n\r" +
+                        $"Tamaño total de la unidad: {Convert.ToDecimal(disk.TotalSize) / 1073741824} GB \n\r"
+                        );
+                }
+            }
+            return discos;
+        }
+
+    }
+}
---
+++ HardwareSO/Registro.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Win32;
+
+namespace HardwareSO
+{
+    public class Registro
+    {
+        public static string Regedit(string fuente, string llave, int opc, string valor = "")
+        {
+            try
+            {
+                switch (opc)
+                {
+                    case 1:
+                        return Registry.GetValue(fuente, llave, "Registro no encontrado.").ToString();
+
+                    case 2:
+                        Registry.SetValue(fuente, llave, valor);
+                        return Registry.GetValue(fuente, llave, "Registro no encontrado.").ToString();
+                    case 3:
+                        Registry.SetValue(fuente, llave, "Sin definir.");
+                        return Registry.GetValue(fuente, llave, "Registro no encontrado.").ToString();
+                }
+                return "Registro no encontrado.";
+            }
+            catch { return "Registro no encontrado."; }
+        }
+    }
+}
